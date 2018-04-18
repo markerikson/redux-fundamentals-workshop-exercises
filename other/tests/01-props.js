05d3b9bd8c31913816d754dc169e8aa4c988db5c
Mark Erikson <mark@isquaredsoftware.com>
2018-04-17 21:08:40 -0400

    Remove unwanted tests

--- other/tests/01-props.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render} from 'enzyme'
-const SayHello = require(`../../src/${process.env.TEST_DIR ||
-  'exercises'}/01-props`).default
-
-test('Renders "Hello {props.firstName} {props.lastName}" in a div', () => {
-  expect(render(<SayHello firstName="Jill" />)).toMatchSnapshot()
-})
--- other/tests/03-styling.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render} from 'enzyme'
-const BoxExample = require(`../../src/${process.env.TEST_DIR ||
-  'exercises'}/03-styling`).Example
-
-test('Renders three styled boxes', () => {
-  expect(render(<BoxExample />)).toMatchSnapshot()
-})
